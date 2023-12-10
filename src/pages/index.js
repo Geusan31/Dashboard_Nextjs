732c87c46458bca9d0057541d3033eeb3d9c2a68
Geusan31 <edurais.ariadaffa@gmail.com>
2023-12-10 19:29:39 +0700

    add get data doa and redirect login when without token

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,20 @@
-// pages/index.js
-
 import { DashboardLayout } from "@/components/layouts";
 import UsersFeatures from "@/features/products";
-import { useAuth } from "@/hooks/auth"; // Gunakan hook untuk pengecekan otentikasi
-import { Loading } from "@/components/loading";
-import LoginPage from "@/features/authentication";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { user, loading } = useAuth(); // Dapatkan status otentikasi dari hook
-
-  // Jika sedang memuat, tampilkan pesan loading
-  if (loading) {
-    return <Loading />;
-  }
-
-  // Jika pengguna belum login, tampilkan halaman login
-  if (!user) {
-    return <LoginPage />;
-  }
-
-  // Jika pengguna sudah login, arahkan ke halaman dashboard
-  return <UsersFeatures />;
+  const router = useRouter();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push({ pathname: "/login" });
+    }
+  }, [router]);
+  return (
+    <>
+      <UsersFeatures />
+    </>
+  );
 }
 
 Home.getLayout = function getLayout(page) {
